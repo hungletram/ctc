e7918640d709669bbd801becea0b2d45f13eea73
jasonit8 <52004170+jasonit8@users.noreply.github.com>
2020-02-06 14:48:10 +0700

    Update milestone_xizach.py

--- milestone_xizach.py
+++ milestone_xizach.py
@@ -1,15 +1,11 @@
 import random
-deck = []
-for i in range(4):
-    deck += [0,2,3,4,5,6,7,8,9,10,10,10,10]
 
 # Chọn bài Xì Dách vì gần gũi với người Việt hơn.
 
 class player():
-    def __init__(self,bet):
+    def __init__(self):
         self.bank = 0
-        self.bet = bet
-        self.bank -= bet
+        self.bet = 0
         self.cards = []
     def value(self):
         if sum(self.cards) > 21:
@@ -31,19 +27,40 @@ class player():
                         s = sum(self.cards) + 11
         return s
 
+def result(player,ai):
+    if ai.value() > player.value():
+        ai.bank += player.bet
+        player.bank -= player.bet
+        return 'loses'
+    elif ai.value() == player.value():
+        return 'draws'
+    else:
+        ai.bank -= player.bet
+        player.bank += player.bet
+        return 'wins'
+
 draw = lambda p: abs(p.value()) < 16
 
-ai = player(0)
+ai = player()
 n = int(input('Number of players: '))
-humans = [player(int(input('Player {} bets: '.format(i+1)))) for i in range(n)]
+humans = [player() for i in range(n)]
 players = humans + [ai]
+dealing = [i for i in range(n+1)]*2
 
 playmore = '1'
 
-while playmore == '1':
-    dealing =[i for i in range(n+1)]*2
+while playmore != '0':
+    deck = []
+    for i in range(4):
+        deck += [0,2,3,4,5,6,7,8,9,10,10,10,10]
+    random.shuffle(deck)
+
+    ai.cards = []
+    for p in range(len(humans)):
+        humans[p].bet = int(input('Player {} bets: '.format(p+1)))
+        humans[p].cards = []
     for p in dealing:
-        players[p].cards += [deck.pop(random.randint(0,len(deck)-1))]
+        players[p].cards += [deck.pop(0)]
 
     announce = []
 
@@ -53,11 +70,12 @@ while playmore == '1':
         else:
             # print('Your cards: {} = {}.'.format(you,value(you)))
             while draw(humans[p]):
-                humans[p].cards += [deck.pop(random.randint(0,len(deck)-1))]
-            draw1 = int(input('Player {}: {} = {}. Draw more (0 = no, 1 = yes)? '.format(p+1,humans[p].cards,humans[p].value()))) if abs(humans[p].value()) < 21 else 0
-            while draw1 != 0:
-                humans[p].cards += [deck.pop(random.randint(0,len(deck)-1))]
-                draw1 = int(input('Player {}: {} = {}. Draw more (0 = no, 1 = yes)? '.format(p+1,humans[p].cards,humans[p].value()))) if abs(humans[p].value()) < 21 else 0
+                humans[p].cards += [deck.pop(0)]
+            if abs(humans[p].value()) < 21:
+                draw1 = str(input('Player {}: {} = {}. Draw more (0 = no, 1 = yes)? '.format(p+1,humans[p].cards,humans[p].value()))) if abs(humans[p].value()) < 21 else 0
+            while abs(humans[p].value()) < 21 and draw1 != '0':
+                humans[p].cards += [deck.pop(0)]
+                draw1 = str(input('Player {}: {} = {}. Draw more (0 = no, 1 = yes)? '.format(p+1,humans[p].cards,humans[p].value()))) if abs(humans[p].value()) < 21 else 0
             else:
                 announce += ['- Player {} cards {} add up to {}.'.format(p+1,humans[p].cards,humans[p].value())]
 
@@ -65,22 +83,10 @@ while playmore == '1':
         print(i)
 
     while draw(ai):
-        ai.cards += [deck.pop(random.randint(0,len(deck)-1))]
+        ai.cards += [deck.pop(0)]
     print('RESULT')
 
     cards = [len(p.cards) for p in humans]
-
-    def result(player,ai=ai):
-        if ai.value() > player.value():
-            ai.bank += player.bet
-            return 'loses'
-        elif ai.value() == player.value():
-            player.bank += player.bet
-            return 'draws'
-        else:
-            ai.bank -= player.bank
-            player.bank += 2*player.bet
-            return 'wins'
 
     while sum(cards) > 0:
         p = cards.index(max(cards))
@@ -88,12 +94,13 @@ while playmore == '1':
         if ai.value() > 0 and ai.value() < 21:
             if len(humans[p].cards) == 2:
                 while ((1-(12/13)**(21-ai.value())) > (ai.value()-16)/6) and (abs(ai.value()) < 21):
-                    ai.cards += [deck.pop(random.randint(0,len(deck)-1))]
+                    ai.cards += [deck.pop(0)]
             else:
                 while ((1-(12/13)**(21-ai.value())) > (1-21/(len(humans[p].cards)*13))) and (abs(ai.value()) < 21):
-                    ai.cards += [deck.pop(random.randint(0,len(deck)-1))]
-        print('- Computers cards {} add up to {}. Player {} {}, your bank = {}.'.format(ai.cards,ai.value(),p+1,result(humans[p]),humans[p].bank))
-    playmore = str(input('Play more? (0 = no, 1 = yes) '))
+                    ai.cards += [deck.pop(0)]
+        print('- Computers cards {} add up to {}. Player {} {}, your bank = {}.'.format(ai.cards,ai.value(),p+1,result(humans[p],ai),humans[p].bank))
+    playmore = str(input('\nPlay more? (0 = no, 1 = yes) '))
+    print()
 else:
     print('FINAL RESULTS')
     print('Computer has {} in the bank.'.format(ai.bank))
