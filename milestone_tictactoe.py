b3c978656826a2eb223ea341116dee38a397ec07
jasonit8 <52004170+jasonit8@users.noreply.github.com>
2020-02-06 13:46:32 +0700

    Add files via upload

---
+++ milestone_tictactoe.py
@@ -0,0 +1,35 @@
+def tictactoe():
+    position = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+    board = lambda p: print('+-----------+\n| {} | {} | {} |\n|---|---|---|\n| {} | {} | {} |\n|---|---|---|\n| {} | {} | {} |\n+-----------+'.format(p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8]))
+    print('WELCOME TO TIC-TAC-TOE!')
+    board([i for i in range(1,10)])
+    pick_marker = str(input('Player 1 picks (X/O): ')).upper()
+    m1 = 'X' if pick_marker != 'O' else 'O'
+    m2 = 'O' if m1 == 'X' else 'X'
+    def nextmove(position):
+        return ' ' in position
+    def winning(position):
+        p = []
+        for i in position:
+            p += [i] if i != ' ' else ['']
+        c1 = ''.join([p[0],p[1],p[2]]) in ['XXX','OOO']
+        c2 = ''.join([p[3],p[4],p[5]]) in ['XXX','OOO']
+        c3 = ''.join([p[6],p[7],p[8]]) in ['XXX','OOO']
+        c4 = ''.join([p[0],p[3],p[6]]) in ['XXX','OOO']
+        c5 = ''.join([p[1],p[4],p[7]]) in ['XXX','OOO']
+        c6 = ''.join([p[2],p[5],p[8]]) in ['XXX','OOO']
+        c7 = ''.join([p[0],p[4],p[8]]) in ['XXX','OOO']
+        c8 = ''.join([p[2],p[4],p[6]]) in ['XXX','OOO']
+        return any([c1,c2,c3,c4,c5,c6,c7,c8])
+    phase = 1
+    while nextmove(position) and not winning(position):
+        board(position)
+        p = int(input("Player {}'s turn (1-9): ".format([0,1,2][phase])))
+        while position[p-1] != ' ':
+            p = int(input("Occupied space, pick another (1-9): "))
+        position[p-1] = ['',m1,m2][phase]
+        phase *= -1
+    else:
+        board(position)
+        return 'Player {} wins.'.format([0,1,2][phase]) if winning(position) else "It's a draw."
+print(tictactoe())
---
+++ milestone_xizach.py
@@ -0,0 +1,92 @@
+import random
+deck = []
+for i in range(4):
+    deck += [0,2,3,4,5,6,7,8,9,10,10,10,10]
+
+# Chọn bài Xì Dách vì gần gũi với người Việt hơn.
+
+class player():
+    def __init__(self,bet):
+        self.bank = 0
+        self.bet = bet
+        self.bank -= bet
+        self.cards = []
+    def value(self):
+        if sum(self.cards) > 21:
+            s = -21
+        elif sum(self.cards) == 0:
+            s = 21
+        else:
+            if len(self.cards) == 5 and sum(self.cards) <= 21:
+                s = 100
+            else:
+                if 0 not in self.cards:
+                    s = sum(self.cards)
+                else:
+                    if sum(self.cards) > 11:
+                        s = sum(self.cards) + self.cards.count(0)
+                    elif sum(self.cards) == 11:
+                        s = sum(self.cards) + 10
+                    else:
+                        s = sum(self.cards) + 11
+        return s
+
+n = int(input('Number of players: '))
+humans = [player(int(input('Player {} bets: '.format(i+1)))) for i in range(n)]
+ai = player(0)
+
+players = humans + [ai]
+dealing =[i for i in range(n+1)]*2
+for p in dealing:
+    players[p].cards += [deck.pop(random.randint(0,len(deck)-1))]
+
+def result(player,ai=ai):
+    if ai.value() > player.value():
+        ai.bank += player.bet
+        return 'loses'
+    elif ai.value() == player.value():
+        player.bank += player.bet
+        return 'draws'
+    else:
+        player.bank += 2*player.bet
+        return 'wins'
+
+draw = lambda p: abs(p.value()) < 16
+
+announce = []
+
+for p in range(len(humans)):
+
+    if humans[p].value() == 21:
+        announce += ['- Player {} cards {} add up to {}.'.format(p+1,humans[p].cards,humans[p].value())]
+    else:
+        # print('Your cards: {} = {}.'.format(you,value(you)))
+        while draw(humans[p]):
+            humans[p].cards += [deck.pop(random.randint(0,len(deck)-1))]
+        draw1 = int(input('Player {}: {} = {}. Draw more (0 = no, 1 = yes)? '.format(p+1,humans[p].cards,humans[p].value()))) if abs(humans[p].value()) < 21 else 0
+        while draw1 != 0:
+            humans[p].cards += [deck.pop(random.randint(0,len(deck)-1))]
+            draw1 = int(input('Player {}: {} = {}. Draw more (0 = no, 1 = yes)? '.format(p+1,humans[p].cards,humans[p].value()))) if abs(humans[p].value()) < 21 else 0
+        else:
+            announce += ['- Player {} cards {} add up to {}.'.format(p+1,humans[p].cards,humans[p].value())]
+
+for i in announce:
+    print(i)
+
+while draw(ai):
+    ai.cards += [deck.pop(random.randint(0,len(deck)-1))]
+print('RESULT')
+
+cards = [len(p.cards) for p in humans]
+
+while sum(cards) > 0:
+    p = cards.index(max(cards))
+    cards[p] = 0
+    if ai.value() > 0 and ai.value() < 21:
+        if len(humans[p].cards) == 2:
+            while ((1-(12/13)**(21-ai.value())) > (ai.value()-16)/6) and (abs(ai.value()) < 21):
+                ai.cards += [deck.pop(random.randint(0,len(deck)-1))]
+        else:
+            while ((1-(12/13)**(21-ai.value())) > (1-21/(len(humans[p].cards)*13))) and (abs(ai.value()) < 21):
+                ai.cards += [deck.pop(random.randint(0,len(deck)-1))]
+    print('- Computers cards {} add up to {}. Player {} {}, your bank = {}.'.format(ai.cards,ai.value(),p+1,result(humans[p]),humans[p].bank))
